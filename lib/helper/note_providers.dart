33c2f3023fc3bac0e3b8253789ef439d39d3478f
Awais Ibrahim <maharawais348@gmail.com>
2021-01-08 20:05:48 +0500

    note_list_screen

--- lib/helper/note_providers.dart
+++ lib/helper/note_providers.dart
@@ -16,5 +16,46 @@ class NoteProvider with ChangeNotifier {
     )
         .toList();
     notifyListeners();
+    @override
+    Widget build(BuildContext context) {
+      return FutureBuilder(
+        future: Provider.of<NoteProvider>(context,listen: false).getNotes(),
+        builder: (context,snapshot)
+        {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+          else
+          {
+            if(snapshot.connectionState == ConnectionState.done)
+            {
+              return Scaffold(
+                body: Consumer<NoteProvider>(
+                  child: noNotesUI(context),
+                  builder: (context, noteprovider, child) =>
+                  noteprovider.items.length <= 0
+                      ? child
+                      : Container(),
+                ),
+                floatingActionButton: FloatingActionButton(
+                  onPressed: () {
+                    goToNoteEditScreen(context);
+                  },
+                  child: Icon(Icons.add),
+                ),
+              );
+            }
+            return Container(
+              width: 0.0,
+              height: 0.0,
+            );
+          }
+        },
+      );
+    }
   }
 }
