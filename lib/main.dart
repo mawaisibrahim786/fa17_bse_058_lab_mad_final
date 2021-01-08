43e52d638f8de715f387b2a33645d48066f9dab1
Awais Ibrahim <maharawais348@gmail.com>
2021-01-08 19:38:04 +0500

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
-
-import 'Notes.dart';
-
+import 'package:flutter_notes/helper/note_provider.dart';
+import 'package:flutter_notes/screens/note_list_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_notes/screens/note_edit_screen.dart';
+import 'package:flutter_notes/screens/note_view_screen.dart';
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: NotesPage(),
+    return ChangeNotifierProvider.value(
+      value: NoteProvider(),
+      child: MaterialApp(
+        title: "Flutter Notes",
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => NoteListScreen(),
+          NoteViewScreen.route: (context) => NoteViewScreen(),
+          NoteEditScreen.route: (context) => NoteEditScreen(),
+        },
+      ),
     );
   }
 }
